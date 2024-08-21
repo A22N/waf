2e26be3a2400d95dc5665a47afa090794aa75f4f
A22N <83749847+A22N@users.noreply.github.com>
2024-08-22 02:10:58 +0700

    test21

--- src/WEB/func/conect.php
+++ src/WEB/func/conect.php
@@ -2,7 +2,7 @@
 <?php
 //$con = new mysqli("localhost","root","","web",3306);
 //build docker dùng dòng 5
-$con = new mysqli("db","melp","1234","web",3306);
+$con = new mysqli("db","melp","P8NhrX2ZRWICk29A3rERbfCUgZhWHdBy","web",3306);
 if($con->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
